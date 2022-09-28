a3f1a77bf9d6fd70e5302a1c50c5770387308dab
kenzzo99 <aknez1406@gmail.com>
2022-09-28 13:24:57 -0400

    Project 2

--- src/types/merkle.rs
+++ src/types/merkle.rs
@@ -1,29 +1,148 @@
+use std::convert::TryInto;
 use super::hash::{Hashable, H256};
+use ring::{digest};
+extern crate bincode;
+use bincode::{serialize, deserialize};
 
+use crate::types::address::Address;
 /// A Merkle tree.
 #[derive(Debug, Default)]
 pub struct MerkleTree {
+    hashes: Vec<Vec<H256>>, // do these need to be type Vec<H256> ??
+
 }
 
 impl MerkleTree {
     pub fn new<T>(data: &[T]) -> Self where T: Hashable, {
-        unimplemented!()
+        let mut len = data.len(); // number of files to store in a merkle tree
+        let mut level = 0; // current level we're populating
+        let mut pointer: usize = 0;
+        let mut hashes: Vec<Vec<H256>> = Vec::new();
+        // hash all the provided data
+        // if input is empty return an empty Vec of Vec<H256>
+        if len > 0 {
+            hashes.push(Vec::new());
+        } else { return MerkleTree { hashes } }
+        // if input contains just one element return a tree with just the hash of the element
+        if len == 1 {
+            hashes[0].push(data[0].hash());
+            return MerkleTree { hashes }
+        }
+
+        // populate the first level of the tree with hashes of data
+        for i in 0..len {
+            hashes[level].push(data[i].hash());
+        };
+
+        // if there's an odd number of hashes, we duplicate the last one to make the number even
+        if len % 2 == 1 {
+            len = len + 1;
+            let temp = hashes[level][hashes[level].len() - 1].clone();
+            hashes[level].push(temp);
+        };
+        // add another level to the tree
+        hashes.push(Vec::new());
+        level = level + 1;
+        // loop until the length of the current level of the tree is 1, i.e. until we compute
+        // the root
+
+        loop {
+            // compute the hash of pairs of hashes, incrementing the pointer along the way
+            for i in 0..len/2  {
+
+                // create a hash of two child hashes
+                let mut ctx = digest::Context::new(&digest::SHA256);
+                ctx.update(hashes[level - 1][i].as_ref()); // maybe will not work
+                ctx.update(hashes[level - 1][i + 1].as_ref());
+                let temp: H256 = ctx.finish().into();
+                hashes[level].push(temp);
+            };
+
+            // halve the size as the current level is half the size of the one before;
+            len = len / 2;
+
+
+            // if the length of the upper level is odd, check if we reached the root, if so break
+            // if not, create a duplicate of the last node in the tree and increment the length
+            if len % 2 == 1 {
+                if len == 1 { break };
+                let temp = hashes[level][len - 1].clone();
+                hashes[level].push(temp);
+                len = len + 1;
+            };
+
+            // switch to upper level
+            level = level + 1;
+            hashes.push(Vec::new());
+
+        };
+        // return the tree
+        MerkleTree { hashes }
     }
 
     pub fn root(&self) -> H256 {
-        unimplemented!()
+        self.hashes[self.hashes.len() - 1][0]
     }
 
     /// Returns the Merkle Proof of data at index i
     pub fn proof(&self, index: usize) -> Vec<H256> {
-        unimplemented!()
+        let mut proof: Vec<H256> = Vec::new(); // vector to return
+        let mut pointer: usize = index;
+
+        if self.hashes.len() == 1 {
+            if self.hashes[0].len() == 1 {
+                return proof
+            }
+            else if index % 2 == 0 {
+                proof.push(self.hashes[0][1]);
+                return { proof }
+            } else {
+                proof.push(self.hashes[0][0]);
+                return { proof }
+            };
+        }
+
+        for i in 0..(self.hashes.len() - 1) {
+            if pointer % 2 == 0 {
+                proof.push(self.hashes[i][pointer + 1]);
+                pointer = pointer + 1;
+            }
+            else {
+                proof.push(self.hashes[i][pointer - 1]);
+                pointer - 1;
+            }
+            pointer = pointer / 2;
+        };
+
+        proof
+
     }
 }
 
 /// Verify that the datum hash with a vector of proofs will produce the Merkle root. Also need the
 /// index of datum and `leaf_size`, the total number of leaves.
 pub fn verify(root: &H256, datum: &H256, proof: &[H256], index: usize, leaf_size: usize) -> bool {
-    unimplemented!()
+    let mut test: &H256 = datum;
+    let mut temp: H256;
+    let mut pointer = index;
+    for i in 0..proof.len() {
+        let mut ctx = digest::Context::new(&digest::SHA256);
+        if pointer % 2 == 0 {
+            ctx.update(test.as_ref());
+            ctx.update(&proof[i].as_ref());
+        } else {
+            ctx.update(&proof[i].as_ref());
+            ctx.update(test.as_ref());
+        }
+        pointer = pointer / 2;
+
+        temp = ctx.finish().into();
+        test = &temp;
+    }
+    if root == test {
+        return true
+    }
+    return false
 }
 // DO NOT CHANGE THIS COMMENT, IT IS FOR AUTOGRADER. BEFORE TEST
 
